aa7adfc334a9c12c3be344da5ce30c706e671888
Excel Okechukwu <excel@taficasa.com>
2025-08-08 13:40:17 +0100

    remove debug

--- backend/backend.py
+++ backend/backend.py
@@ -105,4 +105,4 @@ def process():
     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5200, debug=True)
+    app.run(host='0.0.0.0', port=5200)
